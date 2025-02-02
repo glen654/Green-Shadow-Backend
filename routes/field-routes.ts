9ba7d2c7d9778943c69bce4efdcf0a85898bf9f8
glen654 <glenalloy@gmail.com>
2025-02-02 12:54:39 +0530

    debugging field update route

--- routes/field-routes.ts
+++ routes/field-routes.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import multer from 'multer'
-import {addField} from "../database/field-data-store";
+import {addField, deleteField, updateField} from "../database/field-data-store";
 import {Field} from "../models/Field";
+import FieldModel from "../models/Field-model";
 
 const router = express.Router();
 
@@ -30,4 +31,30 @@ router.post("/add", upload.single("fieldImage"), async (req, res) => {
     }
 })
 
+router.put("/update/:fieldName", upload.single("fieldImage"), async (req,res) => {
+    const fieldName:string = req.params.fieldName;
+
+    const existingField = await FieldModel.findOne({fieldName:fieldName});
+    if(!existingField){
+        return res.status(400).json({message: "Field not found"});
+    }
+    const field:Field = {
+        fieldName: req.body.fieldName || existingField.fieldName,
+
+        location: req.body.location || existingField.location,
+        extentSize: req.body.extentSize ? Number(req.body.extentSize) : existingField.extentSize,
+        fieldImage: req.file? req.file.filename : existingField.fieldImage
+    }
+    try {
+        const updatedField = await updateField(fieldName,field);
+        res.json(updatedField);
+    }catch(err){
+        console.log("Error Updating field",err);
+        res.status(400).send("Error updating field");
+    }
+})
+
+router.delete("/delete/:fieldName",async (req,res)=>{
+
+}
 export default router;
